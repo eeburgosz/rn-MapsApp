48044e033f25bee452ccf730dc422fc2bac2b8cf
eeburgosz <hdst.bur@gmail.com>
2024-05-18 19:09:01 -0300

    Revisar permiso de ubicación

--- src/actions/permissions/location.ts
+++ src/actions/permissions/location.ts
@@ -1,8 +1,9 @@
-//! Solicitud de un permiso.
+//! Solicitud de un permiso. Lógica de negocio
 
 import {
   PERMISSIONS,
   PermissionStatus as RNPermissionStatus,
+  check,
   openSettings,
   request,
 } from 'react-native-permissions';
@@ -37,3 +38,24 @@ export const requestLocationPermission =
 
     return permissionMapper[status] ?? 'unavailable';
   };
+
+export const checkLocationPermission = async (): Promise<PermissionStatus> => {
+  let status: RNPermissionStatus = 'unavailable';
+  if (Platform.OS === 'ios') {
+    status = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+  } else if (Platform.OS === 'android') {
+    status = await check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+  } else {
+    throw new Error('Platform not supported');
+  }
+
+  const permissionMapper: Record<RNPermissionStatus, PermissionStatus> = {
+    granted: 'granted',
+    denied: 'denied',
+    blocked: 'blocked',
+    unavailable: 'unavailable',
+    limited: 'limited',
+  };
+
+  return permissionMapper[status] ?? 'unavailable';
+};
